bc09f82b23c3039aceb9cae749121ae8490a307e
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-02-11 16:55:36 +0000

    I added a new feature for the todo list. A Clear local storage button. This button will clear the local storage and refresh the page. I added a remove button for local storage. This button will remove the item from the local storage and refresh the page. Having trouble with the remove button cant get it to work properly. And i added a feature for displaying the local storage items on the page when the page is loaded.
    restructure html.
    
    restructure the js file. And apply comments to the code. For better understanding.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,43 @@
-const itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : []
+// Const
+const itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+const clearLocalStorageButton = document.getElementById('clearLocalStorage');
+const removeItemButton = document.getElementById('removeItem');
+const displayArea = document.getElementById('displayArea');
+
+// Create a new item
+
+document.querySelector('#enter').addEventListener('click', () => {
+  const item = document.querySelector('#item')
+  createItem(item)
+})
+function createItem(item){
+  itemsArray.push(item.value)
+  localStorage.setItem('items', JSON.stringify(itemsArray))
+  location.reload()
+}
+
+// Remove an item and clear local storage
+
+removeItemButton.addEventListener('click', () => {
+  localStorage.removeItem(item.value,1)
+  location.reload()
+});
+
+clearLocalStorageButton.addEventListener('click', () => {
+  localStorage.clear();
+  location.reload()
+});
+
+
+// Display all items
+
+for (let i = 0; i < localStorage.length; i++) {
+  const key = localStorage.key(i);
+  const value = localStorage.getItem(key);
+
+  const item = document.createElement('div');
+  item.innerHTML = `<li>${key}: ${value}</li>`;
+  displayArea.appendChild(item);
+}
 
 console.log(itemsArray)
-
-document.addEventListener('click', () => {
-    const item = document.querySelector('#item')
-    createItem(item)
-})
-
-function createItem(item) {
-    itemsArray.push(item.value)
-    localStorage.setItem('items', JSON.stringify(itemsArray))
-    
-}
