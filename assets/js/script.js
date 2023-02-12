bf7555d1a2f756b0a42823b8c5cb55f041c2d2c0
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-02-12 17:24:58 +0000

    missing some semi-colons in the script.js file. I added them in and now it works.

--- assets/js/script.js
+++ assets/js/script.js
@@ -56,13 +56,13 @@ function activateRemoveItem(){
       deleteItem(i);
     });
   });
-}
+};
 
 function deleteItem(i){
   itemsArray.splice(i, 1);
   localStorage.setItem('items', JSON.stringify(itemsArray));
   location.reload();
-}
+};
 
 // Save item
 
